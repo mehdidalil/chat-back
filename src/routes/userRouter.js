f3a008249085ee673907d604d2155dc2260711e7
Mayday <mdalil@student.42.fr>
2020-02-19 12:39:44 +0100

    Added user get route and modified query for post route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -5,14 +5,21 @@ import { pgClient } from '../config';
 
 const userRouter = express.Router();
 
+userRouter.get('/', (req, res) => {
+	pgClient
+	.query()
+});
+
 userRouter.post('/create', (req, res) => {
 	try {
 		futaba(userSchema, req.body);
 		const { username, password, mail } = req.body;
 
 		pgClient
-		.query(`INSERT INTO "user" ("username", "password", "mail") VALUES ('${username}', '${password}', '${mail}');`)
+		.query(`INSERT INTO "user" ("username", "password", "mail") VALUES ('${username}', '${password}', '${mail}') RETURNING "id";`)
 		.then(response => {
+			const id = response.rows[0];
+			console.log(id);
 			res.status(200).send("User created !");
 		})
 		.catch(e => {
